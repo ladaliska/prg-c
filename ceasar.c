07913acf36c82d00b08d6d8cb5dc767856a02aec
ladaliska <76939998+ladaliska@users.noreply.github.com>
2021-05-14 10:00:07 +0200

    Add files via upload

---
+++ ceasar.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int caesar (char sifra[], int p) {
+  int i = 0;
+
+  while (sifra[i] != '\0') {
+    if ((sifra[i] + p) >= 65 && (sifra[i] + p) <= 90) {
+      sifra[i] += (p);
+    }
+    else {
+      sifra[i] += (p - 25); 
+    }
+    i++;
+  }
+  printf("%s", sifra);
+  return 0;
+}
+
+int main () {
+char sifra[50];
+int p;
+
+  printf("Zapiste vetu, kterou chcete zakodovat (POUZE VELKA PISMENA): ");
+  scanf("%s", sifra);
+
+  printf("O kolik mist? (1-10): ");
+  scanf("%d", &p);
+
+  caesar (sifra, p);
+
+  return 0;
+}
+
